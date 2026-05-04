803c655f300a5db80dbf5f119dabf9b91089ebd8
Dmitry Openkov <dimaopen@gmail.com>
2026-05-04 16:50:25 +0300

    Added proxying

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import logging
 import json
+import os
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 
 with open('config/config.json', 'r', encoding='utf-8') as config_file:
     config = json.load(config_file)
@@ -11,7 +13,12 @@ with open('config/templates.json', 'r', encoding='utf-8') as templates_file:
 
 logging.basicConfig(level=config['loggingLevel'])
 
-bot = Bot(token=config['token'])
+proxy = config.get('proxy') or os.environ.get('HTTPS_PROXY')
+
+    # Создаем сессию с прокси
+session = AiohttpSession(proxy=proxy)
+
+bot = Bot(token=config['token'], session=session)
 dp = Dispatcher()
 
 bot_id = bot.id
